ab92ce308edf5dc923abda460be4b866fb9f7900
Nykyta Koblianskyi <nkoblianskyi@gmail.com>
2025-06-17 17:25:20 +0200

    1

--- components/header.tsx
+++ components/header.tsx
@@ -6,11 +6,11 @@ export function Header() {
     <div style={{ background: "rgba(0, 0, 0, 0.15)" }}>
       <header className="text-white h-[83px]">
         <div className="container mx-auto px-2 sm:px-4 flex items-center justify-center h-20">
-          <Link href="/" className="flex items-center gap-1 sm:gap-2 lg:gap-3">
-            <div className="w-6 h-6 sm:w-8 sm:h-8 lg:w-10 lg:h-10 bg-green-600 rounded-lg flex items-center justify-center flex-shrink-0">
-              <Image src="/logo.png" alt="Irish Flag" width={16} height={16} className="sm:w-5 sm:h-5 lg:w-6 lg:h-6" />
+          <Link href="/" className="flex items-center gap-1">
+            <div className="w-10 h-10 sm:w-8 sm:h-8 lg:w-14 lg:h-14 bg-transparent rounded-full flex items-center justify-center flex-shrink-0">
+              <Image src="/logo.png" alt="Irish Flag" width={60} height={60} className="sm:w-16 sm:h-16 lg:w-16 lg:h-16" />
             </div>
-            <h1 className="text-xs sm:text-lg md:text-xl lg:text-2xl font-bold whitespace-nowrap">
+            <h1 className="text-xs sm:text-lg md:text-xl lg:text-3xl font-bold whitespace-nowrap">
               <span className="text-green-400">BEST </span>
               <span className="text-white">IRISH </span>
               <span className="text-orange-500">BETTING </span>
